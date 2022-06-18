9fe2f5e4c105e79776311619520dc5a1181d7e9f
hashedpip <103964110+hashedpip@users.noreply.github.com>
2022-06-18 17:34:13 -0300

    added theme assets

---
+++ assets/theme/components/breadcrumbs.js
@@ -0,0 +1,17 @@
+import colors from "assets/theme/base/colors";
+import typography from "assets/theme/base/typography";
+
+const { grey, text, info } = colors;
+const { size } = typography;
+
+export default {
+  styleOverrides: {
+    li: {
+      lineHeight: 0,
+    },
+
+    separator: {
+      fontSize: size.sm,
+    },
+  },
+};
---
+++ assets/theme/functions/linearGradient.js
@@ -0,0 +1,8 @@
+function linearGradient(color, colorState, angle) {
+  if (angle === undefined) {
+    angle = 310;
+  }
+  return `linear-gradient(${angle}deg, ${color}, ${colorState})`;
+}
+
+export default linearGradient;
